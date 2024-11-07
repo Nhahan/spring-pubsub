5b6f2619db36ce4414ba35ab46f88de278c08b0a
Nhahan <kisy324@naver.com>
2024-11-08 00:02:29 +0900

    fix: Dockerfiles and add run script
    Modified the Dockerfiles for 'webflux-module' and 'mvc-module' to correctly copy JAR files from the 'build/libs' directory within their respective module directories. Introduced a new 'run.sh' script to automate project building and Docker Compose operations, facilitating easier project setup and container management.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Building project..."
+./gradlew clean :mvc-module:bootJar :webflux-module:bootJar
+
+echo "Starting Docker Compose..."
+docker-compose down
+docker-compose up --build -d
+
+echo "Docker containers are up and running:"
+docker-compose ps
