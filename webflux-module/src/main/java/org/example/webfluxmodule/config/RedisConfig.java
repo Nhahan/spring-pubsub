952f7aa8d95e93f6a119fb6353530885fc1a2a22
Nhahan <kisy324@naver.com>
2024-11-08 17:09:44 +0900

    chore: remove unused ReactiveRedisTemplate bean

--- webflux-module/src/main/java/org/example/webfluxmodule/config/RedisConfig.java
+++ webflux-module/src/main/java/org/example/webfluxmodule/config/RedisConfig.java
@@ -3,10 +3,7 @@ package org.example.webfluxmodule.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -16,17 +13,4 @@ public class RedisConfig {
             ReactiveRedisConnectionFactory connectionFactory) {
         return new ReactiveRedisMessageListenerContainer(connectionFactory);
     }
-
-    @Bean
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
-        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext.<String, String>newSerializationContext(new StringRedisSerializer())
-                .key(new StringRedisSerializer())
-                .value(new StringRedisSerializer())
-                .hashKey(new StringRedisSerializer())
-                .hashValue(new StringRedisSerializer())
-                .build();
-
-        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
-    }
-
 }
