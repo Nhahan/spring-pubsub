a96d78aed4f193f8df9f3852b5266b87e2f65697
Nhahan <kisy324@naver.com>
2024-11-08 12:07:08 +0900

    feat: publish message endpoint to `TestController`

--- test-module/src/main/java/org/example/testmodule/TestController.java
+++ test-module/src/main/java/org/example/testmodule/TestController.java
@@ -1,20 +1,28 @@
 package org.example.testmodule;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 public class TestController {
 
     private final SseClientService sseClientService;
+    private final StringRedisTemplate redisTemplate;
 
     @GetMapping("/topics/{topic}/test")
     public String test(@PathVariable String topic, @RequestParam int messageCount) throws InterruptedException {
         sseClientService.start(topic, messageCount);
         return sseClientService.getDurations();
+    }
+
+    @PostMapping("/topics/{topic}/publish")
+    public String publish(@PathVariable String topic) {
+        String uuid = UUID.randomUUID().toString();
+        redisTemplate.convertAndSend(topic, uuid);
+        return uuid;
     }
 }
